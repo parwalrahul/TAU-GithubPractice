3a42ca228f353d697b3a36485eedb551a4e7872a
Rahul Parwal <parwalrahul@yahoo.co.in>
2021-07-07 01:02:01 +0530

    Update test.cs

--- GitPractice/GitPractice/test.cs
+++ GitPractice/GitPractice/test.cs
@@ -7,6 +7,7 @@ namespace GitPractice
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! modified");
+            Console.WriteLine("Hello World! modified");
         }
     }
 }
